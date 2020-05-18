67a5dffd7e1f24e43306444b6728957dde9ec55f
Tallot <iolegomg@gmail.com>
2020-05-18 23:44:24 +0300

    Add configuration for MongoDB ReplSet

---
+++ initial_run.sh
@@ -0,0 +1,17 @@
+# run this after setting up the docker-compose
+docker-compose up -d
+sleep 1
+
+# this will initiate replica set for mongodb
+docker exec node1 mongo --eval "
+rs.initiate(
+    {
+    _id: 'rs0',
+    members: [
+        {_id: 0, host: 'node1:27017'},
+        {_id: 1, host: 'node2:27017'},
+        {_id: 2, host: 'node3:27017'}
+        ]
+    }
+)
+"
